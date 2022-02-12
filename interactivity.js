2c274c0ed5816cd7c201834bf5853f6dae6cdde7
Denis <93516910+Bussun@users.noreply.github.com>
2022-02-12 19:30:01 +0100

    restarting from scratch

--- interactivity.js
+++
@@ -1,6 +0,0 @@
-const checkCreditsBtn = document.querySelector('.bottombtn');
-const credits = document.querySelector('.credits-section');
-
-checkCreditsBtn.addEventListener('click', function() {
-    credits.classList.toggle('invisible');
-});
